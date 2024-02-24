ebc65d03da99e82693e69a6175aac027f287fc27
Vineww <vcvicari@gmail.com>
2024-02-24 15:26:37 -0300

    v1.0.0

--- src/main/java/com/sistema/autenticacao/Cadastro.java
+++ src/main/java/com/sistema/autenticacao/Cadastro.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 //Cadastro.java
 public class Cadastro {
 
-    public static void cadastro(){
+    public static void cadastro(User usuario, Password senha){
         //Declarações
         Scanner myObj = new Scanner(System.in);
-        User usuario = new User();
-        Password senha = new Password();
         
         //ID de usuário
         System.out.println("Insira seu nome de usuario: ");
         usuario.setUsuario(myObj.nextLine());
-        RuntimeException.ErroUser(usuario);
 
         //senha de usuário
         System.out.println("Insira sua senha, a senha deve contar pelo menos um caracter maiusculo, numero e caracter especial: ");
         senha.setsenha(myObj.nextLine());
-        RuntimeException.ErroSenha(senha);
+        
 
         myObj.close();
     }
--- src/main/java/com/sistema/autenticacao/Main.java
+++ src/main/java/com/sistema/autenticacao/Main.java
@@ -3,6 +3,11 @@ package com.sistema.autenticacao;
 //Main.java
 public class Main {
     public static void main(String[] args) {
-        Cadastro.cadastro();
+        User usuario = new User();
+        Password senha = new Password();
+
+        Cadastro.cadastro(usuario, senha);
+        RuntimeException.ErroUser(usuario);
+        RuntimeException.ErroSenha(senha);
     }
 }
